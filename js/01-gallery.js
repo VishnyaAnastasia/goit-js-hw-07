1d29da6d3447b57b7ca0c4ee4e32ea1b162bd98b
Anastasia Vishnyakova <vishnya.anastasia@gmail.com>
2022-09-26 18:47:58 +0300

    first-variant

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imgArr = [];
+
+galleryItems.forEach((item) => {
+  const imgElement = `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`;
+  imgArr.push(imgElement);
+});
+
+const imgStr = imgArr.join("");
+
+gallery.insertAdjacentHTML("afterbegin", imgStr);
+
+gallery.addEventListener("click", imgOpenModal);
+function imgOpenModal(event) {
+  event.preventDefault();
+
+  if (event.target === gallery) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: () => window.addEventListener("keydown", imgCloseModalEsc),
+      onClose: () => window.removeEventListener("keydown", imgCloseModalEsc),
+    }
+  );
+
+  instance.show();
+
+  function imgCloseModalEsc(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imgArr = [];
+
+galleryItems.forEach((item) => {
+  const imgElement = `<a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"  title="${item.description}"/></a>`;
+  imgArr.push(imgElement);
+});
+
+const imgStr = imgArr.join("");
+
+gallery.insertAdjacentHTML("afterbegin", imgStr);
+
+gallery.addEventListener("click", imgOpenModal);
+function imgOpenModal(event) {
+  event.preventDefault();
+
+  if (event.target === gallery) {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
+
+  function imgCloseModalEsc(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
