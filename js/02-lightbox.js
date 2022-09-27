1d96b4d604ee03a658d8a2c00b4282a781e30048
Anastasia Vishnyakova <vishnya.anastasia@gmail.com>
2022-09-27 14:38:00 +0300

    new-variant

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,21 +13,6 @@ const imgStr = imgArr.join("");
 
 gallery.insertAdjacentHTML("afterbegin", imgStr);
 
-gallery.addEventListener("click", imgOpenModal);
-function imgOpenModal(event) {
-  event.preventDefault();
-
-  if (event.target === gallery) {
-    return;
-  }
-  var lightbox = new SimpleLightbox(".gallery a", {
-    captionDelay: 250,
-  });
-
-  function imgCloseModalEsc(event) {
-    if (event.code !== "Escape") {
-      return;
-    }
-    instance.close();
-  }
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
